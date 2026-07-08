0a76f00865072d5ed4a51d96e4cc129259a15828
Cassiano Galvao <cassianogalvao2020@gmail.com>
2026-07-08 13:43:20 -0300

    fix: polish mobile storefront

--- app/Views/layout/public_header.php
+++ app/Views/layout/public_header.php
@@ -46,7 +46,7 @@ $headerLinks = $parseNavLinks(\Maia\Helpers\Settings::get('header_nav_links'), [
     <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/design-system.css?v=20260625-6">
     <link rel="stylesheet" href="/assets/css/animations.css?v=20260625-6">
-    <link rel="stylesheet" href="/assets/css/main.css?v=20260708-cart-checkout-2">
+    <link rel="stylesheet" href="/assets/css/main.css?v=20260708-mobile-cards">
     <?= \Maia\Helpers\ScriptInjector::head() ?>
 </head>
 <body>
@@ -66,7 +66,7 @@ $headerLinks = $parseNavLinks(\Maia\Helpers\Settings::get('header_nav_links'), [
                 <span class="input-group__icon">
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>
                 </span>
-                <input class="input" type="search" name="q" placeholder="Buscar produtos, marcas..."
+                <input class="input" type="search" name="q" placeholder="Buscar"
                        value="<?= htmlspecialchars($_GET['q'] ?? '', ENT_QUOTES, 'UTF-8') ?>"
                        aria-label="Buscar produtos" autocomplete="off">
             </div>
